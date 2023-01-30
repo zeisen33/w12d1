526cc867b5e5dc29e58b6338d31eefc01cd1b85b
Zane <zeisen33@gmail.com>
2023-01-30 17:35:56 -0500

    Routes 99%

---
+++ backend/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :pokemon, only: [:show, :update, :index, :create] do 
+      resources :types, only: [:index]
+      resources :items, only: [:index, :create]
+    end
+    resources :items, only: [:update, :destroy]
+  end
+end
